fec0c6433044d70f52187890f904fa3618ffff57
sophiapeep4-ux <sophiapeep4@gmail.com>
2025-09-13 00:04:28 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,30 @@
+function togglemode(){
+  const html = document.documentElement
+  //codigo abaixo é uma maneira mais facil de colocar as condicionais
+  html.classList.toggle("light")
+  //if(html.classList.contains('light')){
+  // html.classList.remove('light')
+  //}else{
+  // html.classList.add('light')}
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode,adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+ 
+
+ 
+
+ 
+}
+
+
+ 
+  
